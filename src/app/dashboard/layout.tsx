fed4f2443be6f000ce2d03229c912cb53af44a81
Prerana Kailash <shecodesherway@gmail.com>
2025-12-26 04:06:42 +0000

    ADD HEADER TO DASHBOARD ALSO

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 import Sidebar from '@/components/dashboard/sidebar';
 import { Button } from '@/components/ui/button';
 import { Menu } from 'lucide-react';
-import { cn } from '@/lib/utils';
+import Header from '@/components/header';
 
 export default function DashboardLayout({
   children,
@@ -32,24 +32,27 @@ export default function DashboardLayout({
   }
 
   return (
-    <div className="flex min-h-screen bg-muted/40">
-      <Sidebar isSidebarOpen={isSidebarOpen} />
-      <div className="flex-1 flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-white px-6">
-            <Button
-                variant="outline"
-                size="icon"
-                className="h-8 w-8"
-                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-            >
-                <Menu className="h-4 w-4" />
-                <span className="sr-only">Toggle Sidebar</span>
-            </Button>
-            <h1 className="text-lg font-semibold">My Learning Journey</h1>
-        </header>
-        <main className="flex-1 p-6">
-            {children}
-        </main>
+    <div className="flex min-h-dvh flex-col">
+      <Header />
+      <div className="flex-1 flex">
+        <Sidebar isSidebarOpen={isSidebarOpen} />
+        <div className="flex-1 flex flex-col">
+          <header className="flex h-14 items-center gap-4 border-b bg-white px-6">
+              <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-8 w-8"
+                  onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              >
+                  <Menu className="h-4 w-4" />
+                  <span className="sr-only">Toggle Sidebar</span>
+              </Button>
+              <h1 className="text-lg font-semibold">My Learning Journey</h1>
+          </header>
+          <main className="flex-1 p-6 bg-muted/40">
+              {children}
+          </main>
+        </div>
       </div>
     </div>
   );
