d6bf7709e719fec239246644870b8115ce5c3940
Prerana Kailash <shecodesherway@gmail.com>
2025-12-27 19:35:58 +0000

    USE THIS APIKEY "AIzaSyCdv5pTx3iNWevAey9rpn5G6HEUF4b4m_8"KEEP IT SAFE AN

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2560173527-17dab",
   "appId": "1:371613558047:web:7b5ab7b8086a5b2ae2bc2a",
-  "apiKey": "AIzaSyCGcMaAoxEaQRTUJXEDTDfOwbcGxXnK_HQ",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-2560173527-17dab.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "371613558047"
