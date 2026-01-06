ce88ae3a0b84d6d0d9794c562b0e6e209c8c32f9
Prerana Kailash <shecodesherway@gmail.com>
2026-01-06 08:29:42 +0000

    new api key "AIzaSyBcfnJX7DVJwgQOIPRC6Q-SnJrwoPJJkcQ"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2560173527-17dab",
   "appId": "1:371613558047:web:7b5ab7b8086a5b2ae2bc2a",
-  "apiKey": "AIzaSyCbg0MlJw99GivyW_dlXzohk-IRWOHhSf4",
+  "apiKey": "AIzaSyBcfnJX7DVJwgQOIPRC6Q-SnJrwoPJJkcQ",
   "authDomain": "studio-2560173527-17dab.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "371613558047"
